59b0626d2c5227384a44886d89ef058c41a7a050
Aravind-Shankar <shankaravind167@gmail.com>
2017-11-08 22:36:02 +0530

    feat: finish seq impl of union find

--- conc_uf.cpp
+++ conc_uf.cpp
@@ -2,25 +2,52 @@
 
 conc_uf::conc_uf(int n)
 {
+	this->n = n;
+	parent = new int[n];
+	size = new int[n];
 
+	for (int i = 0; i < n; ++i)
+	{
+		parent[i] = i;
+		size[i] = 1;
+	}
 }
 
 conc_uf::~conc_uf()
 {
-
+	delete[] parent;
+	delete[] size;
 }
 
 int conc_uf::root(int i)
 {
-	return 1;
+	while (i != parent[i])
+	{
+		parent[i] = parent[parent[i]];
+		i = parent[i];
+	}
+	return i;
 }
 
 bool conc_uf::find(int i, int j)
 {
-	return true;
+	return root(i) == root(j);
 }
 
 void conc_uf::unite(int i, int j)
 {
-
+	int ri = root(i), rj = root(j);
+	if (ri != rj)
+	{
+		if (size[ri] < size[rj])
+		{
+			parent[ri] = rj;
+			size[rj] += size[ri];
+		}
+		else
+		{
+			parent[rj] = ri;
+			size[ri] += size[rj];
+		}
+	}
 }
